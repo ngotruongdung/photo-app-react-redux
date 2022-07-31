866ad3e1c1fe948112e91dce202f6b4ec4eba1e5
Ngô Trường Dũng <85428977+ngotruongdung@users.noreply.github.com>
2022-07-31 23:35:04 +0700

    setup-code

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Col, Container, Row } from "reactstrap";
+import "./Header.scss";
+
+function Header() {
+  return (
+    <header className="header">
+      <Container>
+        <Row className="justify-content-between">
+          <Col xs="auto">
+            <a
+              className="header__link header__title"
+              href="www.google.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Easy FrontEnd
+            </a>
+          </Col>
+          <Col xs="auto">
+            <NavLink
+              exact
+              className="header__link"
+              to="/photos"
+              activeClassName="header__link--active"
+            >
+              Photo App
+            </NavLink>
+          </Col>
+        </Row>
+      </Container>
+    </header>
+  );
+}
+
+export default Header;
